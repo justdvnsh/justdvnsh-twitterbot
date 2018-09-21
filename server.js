befc550ca6e54c0dd82f2eedad218f7641393033
Glitch (justdvnsh-twitterbot) <none>
2018-09-21 05:17:51 +0000

    ☎️🚄 Updated with Glitch

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-'use strict';
+// server.js
+// where your node app starts
 
+// init project
 var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
